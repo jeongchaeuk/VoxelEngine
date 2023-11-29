c35ebe1ea083fe168134b798fdcab054a42b9d63
jeongchaeuk <jeongchaeuk@gmail.com>
2023-11-29 17:26:40 +0900

    Update chunk_mesh_builder.py

--- meshes/chunk_mesh_builder.py
+++ meshes/chunk_mesh_builder.py
@@ -32,7 +32,7 @@ def build_chunk_mesh(chunk_voxels, format_size):
     # Volxel's visible face is 3 (top, front, right).
     # Each face has 2 triangles, each triangle has 3 vertices.
     NUM_VOXEL_VERTICES = 18
-    # Vertex attributes: (x, y, z, voxel_id, face_id)
+    # Vertex attributes's format: x, y, z, voxel_id, face_id
     ARRAY_SIZE = CHUNK_VOL * NUM_VOXEL_VERTICES * format_size
     vertex_data = np.empty(ARRAY_SIZE, dtype='uint8')
     index = 0
@@ -46,65 +46,57 @@ def build_chunk_mesh(chunk_voxels, format_size):
 
                 # Top face.
                 if is_void((x, y + 1, z), chunk_voxels):
-                    # Voxel: (0, 0, 0)
-                    # (0, 1, 0), (1, 1, 0), (1, 1, 1), (0, 1, 1)
-                    # Each vertex's format: x, y, z, voxel_id, face_id
-                    v0 = (x,        y + 1,      z,      voxel_id,   TOP)
-                    v1 = (x + 1,    y + 1,      z,      voxel_id,   TOP)
-                    v2 = (x + 1,    y + 1,      z + 1,  voxel_id,   TOP)
-                    v3 = (x,        y + 1,      z + 1,  voxel_id,   TOP)
+                    v0 = (x,     y + 1, z,     voxel_id, TOP)
+                    v1 = (x + 1, y + 1, z,     voxel_id, TOP)
+                    v2 = (x + 1, y + 1, z + 1, voxel_id, TOP)
+                    v3 = (x,     y + 1, z + 1, voxel_id, TOP)
                     # counter_clock_wise
-                    two_triangles = (v0, v3, v2, v0, v2, v1)
-                    index = add_data(vertex_data, index, *two_triangles)
+                    vertices = (v0, v3, v2, v0, v2, v1)
+                    index = add_data(vertex_data, index, *vertices)
 
                 # Bottom face
                 if is_void((x, y - 1, z), chunk_voxels):
-                    # (0, 0, 0), (1, 0, 0), (1, 0, 1), (0, 0, 1)
-                    v0 = (x,        y,      z,      voxel_id,   BOTTOM)
-                    v1 = (x + 1,    y,      z,      voxel_id,   BOTTOM)
-                    v2 = (x + 1,    y,      z + 1,  voxel_id,   BOTTOM)
-                    v3 = (x,        y,      z + 1,  voxel_id,   BOTTOM)
-                    two_triangles = (v0, v2, v3, v0, v1, v2)
-                    index = add_data(vertex_data, index, *two_triangles)
-
-                # Front face.
-                if is_void((x, y, z + 1), chunk_voxels):
-                    # (0, 1, 1), (1, 1, 1), (1, 0, 1), (0, 0, 1)
-                    v0 = (x,        y + 1,      z + 1,  voxel_id,   FRONT)
-                    v1 = (x + 1,    y + 1,      z + 1,  voxel_id,   FRONT)
-                    v2 = (x + 1,    y,          z + 1,  voxel_id,   FRONT)
-                    v3 = (x,        y,          z + 1,  voxel_id,   FRONT)
-                    two_triangles = (v0, v3, v2, v0, v2, v1)
-                    index = add_data(vertex_data, index, *two_triangles)
-
-                # Back face.
-                if is_void((x, y, z - 1), chunk_voxels):
-                    # (0, 1, 0), (1, 1, 0), (1, 0, 0), (0, 0, 0)
-                    v0 = (x,        y + 1,      z,  voxel_id,   BACK)
-                    v1 = (x + 1,    y + 1,      z,  voxel_id,   BACK)
-                    v2 = (x + 1,    y,          z,  voxel_id,   BACK)
-                    v3 = (x,        y,          z,  voxel_id,   BACK)
-                    two_triangles = (v0, v2, v3, v0, v1, v2)
-                    index = add_data(vertex_data, index, *two_triangles)
+                    v0 = (x,     y, z,     voxel_id, BOTTOM)
+                    v1 = (x + 1, y, z,     voxel_id, BOTTOM)
+                    v2 = (x + 1, y, z + 1, voxel_id, BOTTOM)
+                    v3 = (x,     y, z + 1, voxel_id, BOTTOM)
+                    vertices = (v0, v2, v3, v0, v1, v2)
+                    index = add_data(vertex_data, index, *vertices)
 
                 # Right face.
                 if is_void((x + 1, y, z), chunk_voxels):
-                    # (1, 1, 1), (1, 1, 0), (1, 0, 0), (1, 0, 1)
-                    v0 = (x + 1,    y + 1,      z + 1,  voxel_id,   RIGHT)
-                    v1 = (x + 1,    y + 1,      z,      voxel_id,   RIGHT)
-                    v2 = (x + 1,    y,          z,      voxel_id,   RIGHT)
-                    v3 = (x + 1,    y,          z + 1,  voxel_id,   RIGHT)
-                    two_triangles = (v0, v3, v2, v0, v2, v1)
-                    index = add_data(vertex_data, index, *two_triangles)
+                    v0 = (x + 1, y,     z,     voxel_id, RIGHT)
+                    v1 = (x + 1, y + 1, z,     voxel_id, RIGHT)
+                    v2 = (x + 1, y + 1, z + 1, voxel_id, RIGHT)
+                    v3 = (x + 1, y,     z + 1, voxel_id, RIGHT)
+                    vertices = (v0, v1, v2, v0, v2, v3)
+                    index = add_data(vertex_data, index, *vertices)
 
                 # Left face.
                 if is_void((x - 1, y, z), chunk_voxels):
-                    # (0, 1, 1), (0, 1, 0), (0, 0, 0), (0, 0, 1)
-                    v0 = (x,    y + 1,      z + 1,  voxel_id,   LEFT)
-                    v1 = (x,    y + 1,      z,      voxel_id,   LEFT)
-                    v2 = (x,    y,          z,      voxel_id,   LEFT)
-                    v3 = (x,    y,          z + 1,  voxel_id,   LEFT)
-                    two_triangles = (v0, v2, v3, v0, v1, v2)
-                    index = add_data(vertex_data, index, *two_triangles)
+                    v0 = (x, y,     z,     voxel_id, LEFT)
+                    v1 = (x, y + 1, z,     voxel_id, LEFT)
+                    v2 = (x, y + 1, z + 1, voxel_id, LEFT)
+                    v3 = (x, y,     z + 1, voxel_id, LEFT)
+                    vertices = (v0, v2, v1, v0, v3, v2)
+                    index = add_data(vertex_data, index, *vertices)
 
-    return vertex_data[:index + 1]
+                # Back face.
+                if is_void((x, y, z - 1), chunk_voxels):
+                    v0 = (x,     y,     z, voxel_id, BACK)
+                    v1 = (x,     y + 1, z, voxel_id, BACK)
+                    v2 = (x + 1, y + 1, z, voxel_id, BACK)
+                    v3 = (x + 1, y,     z, voxel_id, BACK)
+                    vertices = (v0, v1, v2, v0, v2, v3)
+                    index = add_data(vertex_data, index, *vertices)
+
+                # Front face.
+                if is_void((x, y, z + 1), chunk_voxels):
+                    v0 = (x,     y,     z + 1, voxel_id, FRONT)
+                    v1 = (x,     y + 1, z + 1, voxel_id, FRONT)
+                    v2 = (x + 1, y + 1, z + 1, voxel_id, FRONT)
+                    v3 = (x + 1, y,     z + 1, voxel_id, FRONT)
+                    vertices = (v0, v2, v1, v0, v3, v2)
+                    index = add_data(vertex_data, index, *vertices)
+
+    return vertex_data[:index]
